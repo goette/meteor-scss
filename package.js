c25ced8c4811f528e1bff5a6a029ba84ad778f9f
Martin Götte <mrtngtt@gmail.com>
2014-06-03 17:27:31 +0200

    Add autoprefixer

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package._transitional_registerBuildPlugin({
     npmDependencies: {
         'node-sass': '0.8.6',
         'lodash': '2.4.1',
-        autoprefixer: 'latest'
+        'autoprefixer': 'latest'
     }
 });
 

8253499b771ad8ec542cee6310be323aa534b918
Martin Götte <mrtngtt@gmail.com>
2014-06-03 17:57:55 +0200

    update ap version

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package._transitional_registerBuildPlugin({
     npmDependencies: {
         'node-sass': '0.8.6',
         'lodash': '2.4.1',
-        'autoprefixer': '1.1.20140523'
+        'autoprefixer': '1.0.20140203'
     }
 });
 
